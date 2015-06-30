2701fe0c56f00fb9440f65d3ac8dee0e7b33bdcb
niro273 <niro273@hotmail.com>
2015-06-30 13:47:10 +0530

    test 1

--- api/Control/ClickSelect.js
+++ api/Control/ClickSelect.js
@@ -106,6 +106,7 @@ BIMSURFER.Control.ClickSelect = BIMSURFER.Class(BIMSURFER.Control, {
         /* Set the node selected to a global variable */
         //console.log("clicked node : " + hit);
         selectedNode = hit.nodeId;
+		//test
 
 		this.highlighted = this.SYSTEM.scene.findNode(hit.nodeId);
 		var groupId = this.highlighted.findParentByType("translate").data.groupId;
